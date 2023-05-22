6344356dce6bcab5d2879bfd181ed38cc40daa37
Anne-lise Emig <anneliseemig@gmail.com>
2023-05-22 18:52:59 -0500

    added files

--- src/server/server.ts
+++ src/server/server.ts
@@ -14,10 +14,6 @@ app.use(cookieParser());
 // serve login page
 app.use(express.static(path.resolve(__dirname, '../client')));
 
-app.get('/api', (req: Request, res: Response) => {
-    res.send('Hello world!')
-})
-
 app.get('/', (req: Request, res: Response) => {
     res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
