e00e4ade1982d03e954b5b859d9aadd2fd5ae814
GaryB93 <garybalogh93@gmail.com>
2023-05-23 11:50:40 -0500

    Submit code event

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -5,8 +5,9 @@ import { javascript } from '@codemirror/lang-javascript';
 import Prompt from './Prompt';
 
 const Home = () => {
-  const [code, setCode] = useState();
-  const [promptData, setPromptData] = useState();
+  const [codeEditorValue, setCodeEditorValue] = useState('');
+  const [promptData, setPromptData] = useState('');
+  const [solutions, setSolutions] = useState();
 
   useEffect(() => {
     // Function to make a request for the latest algorithm and pass it down to the Prompt component
@@ -23,6 +24,29 @@ const Home = () => {
     fetchLatestAlgorithm();
   }, []);
 
+  const handleSubmit = () => {
+    fetch('/path', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        codeEditorValue: codeEditorValue
+      })
+    })
+    .then(res => res.json())
+    .then((data) => {
+      setSolutions(data);
+    })
+    .catch(err => {
+      console.log('Error connecting to server using path \'/path\'');
+    })
+  };
+
+  const handleClear = () => {
+    setCodeEditorValue('');
+  };
+
   return (
     <div className='home-page'>
       <div id='algo-content'>
@@ -32,12 +56,13 @@ const Home = () => {
           id='code-mirror'
           value='//Hello World!'
           theme='dark'
+          onChange={(newCode) => setCode(newCode)}
           extensions={[javascript({ jsx: true }), EditorView.lineWrapping]}
         />
-        <button id='submit-code' name='submit-code' type='button'>
+        <button id='submit-code' name='submit-code' type='button' onClick={handleSubmit}>
           Submit
         </button>
-        <button id='clr-editor' name='clr-editor' type='button'>
+        <button id='clr-editor' name='clr-editor' type='button' onClick={handleClear}>
           Clear
         </button>
       </div>
