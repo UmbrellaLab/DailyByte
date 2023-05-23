adcea6caeaa923c22b1efc25af44b01e9dde5898
GaryB93 <garybalogh93@gmail.com>
2023-05-23 15:09:01 -0500

    Create account logic

--- src/server/controllers/signInController.ts
+++ src/server/controllers/signInController.ts
@@ -60,6 +60,7 @@ export const signInController = {
             const user_id = user.rows.user_id;
             res.cookie('user_id', user_id);
             res.cookie('username', username);
+            res.locals.verified = "true";
             return next();
         } catch (err){
             baseError.log = `Error caught in signInController: ${err}`;
