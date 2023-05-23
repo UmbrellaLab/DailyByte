6d34bbb558c9b7b70bd0708267c38f008555272f
GaryB93 <garybalogh93@gmail.com>
2023-05-22 23:34:01 -0500

    Adding styles to home page

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { EditorView } from '@codemirror/view';
+import { javascript } from '@codemirror/lang-javascript';
 
 const Home = () => {
+  const [code, setCode] = useState();
+
   return (
     <div className='home-page'>
       <div id='algo-content'>
@@ -11,8 +16,12 @@ const Home = () => {
 
         </div>
         <div id='code-editor'>
-          
+          <CodeMirror
+            value='//Hello World!'
+            height='100px'
+            extensions={[javascript({ jsx: true}), EditorView.lineWrapping]}/>
         </div>
+        <button id='submit-code' name='submit-code' type='button'>Submit</button>
       </div>
     </div>
   )
