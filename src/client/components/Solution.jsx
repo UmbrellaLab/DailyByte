1d46f44dde752131297eed7a268f40722b7d1e94
Rachel Kucharski <rachel.kucharski@codesmith.io>
2023-05-24 08:59:48 -0700

    solutions of the day max height
    Co-authored-by: Rachel Kucharski <rachelk585@users.noreply.github.com>
    Co-authored-by: Anne-lise Emig <annelise08@users.noreply.github.com>
    Co-authored-by: Gary Balogh <GaryB93@users.noreply.github.com>
    Co-authored-by: Akeem Smith  <AkeemESmith@users.noreply.github.com>

--- src/client/components/Solution.jsx
+++ src/client/components/Solution.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { CodeSnippet } from '@carbon/react';
 
-const Solution = ({solutionData}): JSX.Element => {
+const Solution = ({solutionData}) => {
   const [stars, setStars] = useState(solutionData.star_count);
 
-  const handleStar = (id: number) => {
+  const handleStar = (id) => {
     fetch('/solutions', {
       method: 'PATCH',
       headers: {
